ac595abc5a91b0b57dd6fad65ab24bc181545fc3
t6677658 <thomasla256@gmail.com>
2019-04-08 09:55:06 +1000

    TST: Added tests for test integrity

--- pyco3dev/included_tests.py
+++ pyco3dev/included_tests.py
@@ -33,6 +33,55 @@ def main(test_dir, alltests):
     else:
         click.secho('All test files are included!', fg='blue')
 
+def checkTestFileIntegrity(filepath):
+    """Checks: (1) method indentation; (2) crippled tests; (3) commented tests"""
+
+    failed_lines = []
+    with open(filepath) as f:
+
+        class_indent = -1
+        funct_indent = -1
+        multi_comment = False
+
+        for line in f:
+            sline = line.lstrip(' ')
+            cur_indent = len(line) - len(sline)
+            if sline.startswith('class'):
+                class_indent = cur_indent
+                funct_indent = -1
+            if sline.startswith('def'):
+                if cur_indent == 0:
+                    failed_lines.append('Not in class: ' + sline)
+                elif funct_indent == -1:
+                    if cur_indent <= class_indent or cur_indent != 4:
+                        failed_lines.append('Indentation: ' + sline)
+                    else:
+                        funct_indent = cur_indent
+                elif cur_indent != funct_indent:
+                    failed_lines.append('Indentation: ' + sline)
+
+                if not ((sline.startswith('def test_') and sline.find('(self):') != -1) or sline.startswith('def setUp(self):')):
+                    failed_lines.append('Crippled: ' + sline)
+
+            if sline.startswith('#') and sline.find('def test_') != -1:
+                failed_lines.append('Commented: ' + sline)
+
+            if multi_comment:
+                def_pos = sline.find('def test_')
+                if def_pos != -1 and def_pos < sline.find('"""'):
+                    failed_lines.append('Commented: ' + sline)
+            while sline.find('"""') != -1:
+                sline = sline[sline.find('"""') + 3:len(sline)]
+                multi_comment = not multi_comment
+
+                if multi_comment:
+                    def_pos = sline.find('def test_')
+                    if def_pos != -1 and def_pos < sline.find('"""'):
+                        failed_lines.append('Commented: ' + sline)
+
+        for err in failed_lines:
+            print(err.rstrip())
+
 
 if __name__ == '__main__':
     main()
---
+++ pyco3dev/test_included_tests.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+from cogent3.util.unit_test import main, TestCase
+
+
+def test_no_class_before(self):
+    self.assertTrue(True)
+
+
+class IncludedTestsTests(TestCase):
+
+def test_bad_first_test(self):
+    self.assertTrue(True)
+
+    def setUp(self):
+        self.assertTrue(True)
+
+    def test_normal0(self):
+        self.assertTrue(True)
+
+    def test_normal1(self):
+        self.assertTrue(True)
+
+        def test_bad_indent(self):
+            self.assertTrue(True)
+
+    def test_normal2(self):
+        self.assertTrue(True)
+
+#    def test_line_comment(self):
+        self.assertTrue(True)
+
+"""  def test_multi_comment(self): """
+#        self.assertTrue(True)
+
+"""  
+    Testing multi_comment edge case 
+    def test_multi_comment(self):
+        self.assertTrue(True)
+"""
+
+    def est_corrupted(self):
+        self.assertTrue(True)
+
+    def test_no_self():
+        # not sure how this would compile...
+        self.assertTrue(True)
+
+    def test_normal3(self):
+        self.assertTrue(True)
+
+
+def test_no_class_after(self):
+    self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    main()
+
