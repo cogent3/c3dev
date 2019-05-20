3a7d23db4bf5affdb83c1c7cc703de25b5a62fd2
t6677658 <thomasla256@gmail.com>
2019-05-20 16:15:53 +1000

    STY: PEP8 test_integrity

--- pyco3dev/integrity_tests.py
+++ pyco3dev/integrity_tests.py
@@ -2,20 +2,19 @@ import pathlib
 import click
 from difflib import SequenceMatcher
 
+
 @click.command()
 @click.option('-d', '--test_dir',
               type=click.Path(exists=True),
               help='root directory for tests')
 def main(test_dir):
-    """identifies nominal test files not included in the test suite"""
+    """verifies test files included in the test suite"""
     if not test_dir.endswith('/'):
         test_dir += '/'  # just simplifies subsequent logic
 
-    test_modules = []
     for path in pathlib.Path(test_dir).rglob('**/test*.py'):
-        print(path.absolute());
-        check_test_file_integrity(path.absolute());
-        print();
+        check_test_file_integrity(path.absolute())
+        print()
 
 
 def check_test_file_integrity(filepath):
@@ -24,51 +23,25 @@ def check_test_file_integrity(filepath):
     failed_lines = []
     with open(filepath) as f:
 
-        class_indent = -1
-        funct_indent = -1
-        multi_comment = False
+        for l in f:
+            line = l.lstrip(' ')
+            cur_indent = len(l) - len(line)
+            if line.startswith('def'):
+                if cur_indent == 0 and line.find('test_') != -1:
+                    failed_lines.append('Not in class: ' + line)
+                if cur_indent != 4 and line.find('test_') != -1:
+                    failed_lines.append('Indentation: ' + line)
 
-        for line in f:
-            sline = line.lstrip(' ')
-            cur_indent = len(line) - len(sline)
-            if sline.startswith('class'):
-                class_indent = cur_indent
-                funct_indent = -1
-            if sline.startswith('def'):
-                if cur_indent == 0 and sline.find('test_') != -1:
-                    failed_lines.append('Not in class: ' + sline)
-                #elif funct_indent == -1:
-                #    if cur_indent <= class_indent:
-                #        failed_lines.append('Indentation K: ' + sline)
-                #    else:
-                #        funct_indent = cur_indent
-                #elif cur_indent != funct_indent:
-                #    failed_lines.append('Indentation M: ' + sline)
-
-                if cur_indent != 4 and sline.find('test_') != -1:
-                    failed_lines.append('Indentation: ' + sline)
-
-                if sline.startswith('def ') and sline.find('_') != -1:
-                    possible_test = sline[4:sline.find('_')]
+                if line.startswith('def ') and line.find('_') != -1:
+                    possible_test = line[4:line.find('_')]
                     delta = SequenceMatcher(None, possible_test, 'test').ratio()
-                    if delta < 1.0 and delta > 0.7: # can tweak this
-                        failed_lines.append('Crippled: ' + sline)
+                    if 0.7 < delta < 1.0:  # can tweak this
+                        failed_lines.append('Crippled: ' + line)
 
-            if sline.startswith('#') and sline.find('def test_') != -1:
-                failed_lines.append('Commented: ' + sline)
+            if line.startswith('#') and line.find('def test_') != -1:
+                failed_lines.append('Commented: ' + line)
 
-            if multi_comment:
-                def_pos = sline.find('def test_')
-                if def_pos != -1 and def_pos < sline.find('"""'):
-                    failed_lines.append('Commented: ' + sline)
-            while sline.find('"""') != -1:
-                sline = sline[sline.find('"""') + 3:len(sline)]
-                multi_comment = not multi_comment
-
-                if multi_comment:
-                    def_pos = sline.find('def test_')
-                    if def_pos != -1 and def_pos < sline.find('"""'):
-                        failed_lines.append('Commented: ' + sline)
+        print(str(filepath.absolute()) + ': ok!' if len(failed_lines) == 0 else ': warnings')
 
         for err in failed_lines:
             print(err.rstrip())
