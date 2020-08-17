8f58a7bf0b18ce60750c80aecbdcfbf764c6b176
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2020-08-17 21:20:37 +1000

    ENHL: added flag for skipping modifying precommit hooks on VCses

--- c3dev/devconfig.py
+++ c3dev/devconfig.py
@@ -42,66 +42,68 @@ def write_config(path, settings):
 @click.argument("c3dev_dir", type=click.Path(exists=True))
 @click.argument("cogent3_dir", type=click.Path(exists=True))
 @click.option("-sj", "--skip_jupyter", is_flag=True, help="skip jupyter config")
-def main(c3dev_dir, cogent3_dir, skip_jupyter):
+@click.option("-sv", "--skip_vc", is_flag=True, help="skip version control config")
+def main(c3dev_dir, cogent3_dir, skip_jupyter, skip_vc):
     """installs jupyter plotly extensions, then configures mercurial.
        Warning: overwrites .git pre-commit and pre-push hooks"""
     c3dev_path = pathlib.Path(c3dev_dir).resolve()
     cogent3_path = pathlib.Path(cogent3_dir).resolve()
     if not skip_jupyter:
         config_jupyter_plotly()
+        
+    if not skip_vc:
+        if (c3dev_path / ".hg").exists():
+            # precommit hooks for c3dev hgrc
+            hg_pyco3 = {"hooks": {}}
+            hg_pyco3["hooks"]["precommit.black"] = "black " + c3dev_path.name
+            hg_pyco3["hooks"]["precommit.isort"] = "isort -rc " + c3dev_path.name
+            pyco_path = c3dev_path / ".hg/hgrc"
+            assert pyco_path.exists()
+            write_config(str(pyco_path), hg_pyco3)
 
-    if (c3dev_path / ".hg").exists():
-        # precommit hooks for c3dev hgrc
-        hg_pyco3 = {"hooks": {}}
-        hg_pyco3["hooks"]["precommit.black"] = "black " + c3dev_path.name
-        hg_pyco3["hooks"]["precommit.isort"] = "isort -rc " + c3dev_path.name
-        pyco_path = c3dev_path / ".hg/hgrc"
-        assert pyco_path.exists()
-        write_config(str(pyco_path), hg_pyco3)
+        if (c3dev_path / ".git").exists():
+            # precommit hooks for c3dev git config
+            pyco3_pre_commit = c3dev_path / ".git/hooks/pre-commit"
+            f = open(pyco3_pre_commit, "w")
+            f.writelines(
+                [
+                    "#!/bin/bash\n",
+                    "black " + c3dev_path.name + "\n",
+                    "isort -rc " + c3dev_path.name,
+                ]
+            )
+            f.close()
+            if not sys.platform == "win32":
+                exec_command("chmod +x " + str(pyco3_pre_commit.absolute()))
 
-    if (c3dev_path / ".git").exists():
-        # precommit hooks for c3dev git config
-        pyco3_pre_commit = c3dev_path / ".git/hooks/pre-commit"
-        f = open(pyco3_pre_commit, "w")
-        f.writelines(
-            [
-                "#!/bin/bash\n",
-                "black " + c3dev_path.name + "\n",
-                "isort -rc " + c3dev_path.name,
-            ]
-        )
-        f.close()
-        if not sys.platform == "win32":
-            exec_command("chmod +x " + str(pyco3_pre_commit.absolute()))
+        if (cogent3_path / ".hg").exists():
+            # precommit hooks for cogent3 hgrc
+            cogent3 = {"hooks": {}}
+            cogent3["hooks"]["pre-push"] = "tox -e py37"
+            cogent3["hooks"]["precommit.black"] = "black tests/ src/"
+            cogent3["hooks"]["precommit.isort"] = "isort -rc tests/ src/"
+            cogent3["hooks"]["pre-push.bookmark"] = "hg bookmark -fr default develop"
+            cogent3path = cogent3_path / ".hg/hgrc"
+            write_config(str(cogent3path), cogent3)
 
-    if (cogent3_path / ".hg").exists():
-        # precommit hooks for cogent3 hgrc
-        cogent3 = {"hooks": {}}
-        cogent3["hooks"]["pre-push"] = "tox -e py38"
-        cogent3["hooks"]["precommit.black"] = "black tests/ src/"
-        cogent3["hooks"]["precommit.isort"] = "isort -rc tests/ src/"
-        cogent3["hooks"]["pre-push.bookmark"] = "hg bookmark -fr default develop"
-        cogent3path = cogent3_path / ".hg/hgrc"
-        write_config(str(cogent3path), cogent3)
+        if pathlib.Path(cogent3_path / ".git").exists():
+            # prepush hooks for c3dev git config
+            cogent3_pre_push = cogent3_path / ".git/hooks/pre-push"
+            f = open(cogent3_pre_push, "w")
+            f.writelines(["#!/bin/bash\n", "tox -e py37"])
+            f.close()
+            if not sys.platform == "win32":
+                exec_command("chmod +x " + str(cogent3_pre_push.absolute()))
 
-    if pathlib.Path(cogent3_path / ".git").exists():
-        # prepush hooks for c3dev git config
-        cogent3_pre_push = cogent3_path / ".git/hooks/pre-push"
-        f = open(cogent3_pre_push, "w")
-        f.writelines(["#!/bin/bash\n", "tox -e py38"])
-        f.close()
-        if not sys.platform == "win32":
-            exec_command("chmod +x " + str(cogent3_pre_push.absolute()))
-
-        # precommit hooks for c3dev git config
-        cogent3_pre_commit = cogent3_path / ".git/hooks/pre-commit"
-        f = open(cogent3_pre_commit, "w")
-        f.writelines(
-            ["#!/bin/bash\n", "black tests/ src/\n", "isort -rc tests/ src/\n",]
-        )
-        f.close()
-        if not sys.platform == "win32":
-            exec_command("chmod +x " + str(cogent3_pre_commit.absolute()))
+            # precommit hooks for c3dev git config
+            cogent3_pre_commit = cogent3_path / ".git/hooks/pre-commit"
+            f = open(cogent3_pre_commit, "w")
+            f.writelines(
+                ["#!/bin/bash\n", "black tests/ src/\n", "isort -rc tests/ src/\n",]
+            )
+            f.close()
+            if not sys.platform == "win32":
+                exec_command("chmod +x " + str(cogent3_pre_commit.absolute()))
 
 
 if __name__ == "__main__":
